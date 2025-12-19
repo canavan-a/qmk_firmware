17e7286015e2b1df321c1caaaf8542244322f7e5
canavan-a <aidan.canavan3@gmail.com>
2025-12-19 13:26:11 -0500

    added custom layer

--- keyboards/keychron/q1_max/ansi_encoder/keymaps/default/keymap.c
+++ keyboards/keychron/q1_max/ansi_encoder/keymaps/default/keymap.c
@@ -22,6 +22,11 @@ enum layers {
     MAC_FN,
     WIN_BASE,
     WIN_FN,
+    NUM_LAYER,
+};
+
+enum custom_keycodes {
+    ALT_SPC = SAFE_RANGE,
 };
 
 // clang-format off
@@ -56,6 +61,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
         _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,            _______,            KC_END,
         _______,            _______,  _______,  _______,  _______,  BAT_LVL,  NK_TOGG,  _______,  _______,  _______,  _______,            _______,  _______,
+        _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______,  _______,  _______),
+
+    [NUM_LAYER] = LAYOUT_ansi_82(
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
+        _______,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     _______,            _______,            _______,
+        _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,
         _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______,  _______,  _______)
 };
 
@@ -73,5 +86,21 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (!process_record_keychron_common(keycode, record)) {
         return false;
     }
+
+    static bool alt_held = false;
+
+    if (keycode == KC_LALT || keycode == KC_RALT) {
+        alt_held = record->event.pressed;
+    }
+
+    if (keycode == KC_SPC && alt_held && record->event.pressed) {
+        layer_on(NUM_LAYER);
+        return false;
+    }
+
+    if (keycode == KC_SPC && !record.event.pressed) {
+        layer_off(NUM_LAYER);
+    }
+
     return true;
 }
